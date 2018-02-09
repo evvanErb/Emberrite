79013d2efc665c2dee86a48331245207b4e4be06
evvanErb <evvan@protonmail.com>
2018-02-09 08:57:01 -0500

    Added gates description

--- mainEmberrite/maps/castle.py
+++ mainEmberrite/maps/castle.py
@@ -15,4 +15,21 @@ class castleRooms:
 		self.gates()
     
     def gates(self):
-        return("GATES")
+        #Description
+		print("\nYou are in a giant iron archway. There are giant oak doors to the East belonging to an even grander castle before you.\nThere is a stone path leading West.")
+		#Player choice
+		choice = raw_input("\n>>> ").lower()
+		if ((choice == "w") or (choice == "west")):
+			return(self.graveyard())
+		elif ((choice == "e") or (choice == "east")):
+			return(self.antiChamber())
+		elif ((choice == "n") or (choice == "north") or (choice == "s") or (choice == "south")):
+			print("\n[!] You can not go that way!")
+			return(self.gates())
+		elif (choice == "look"):
+			return(self.gates())
+
+		#Check for standard option
+		else:
+			print(functions.standardOptions(choice,self.hero,self.inv))
+			return(self.gates())
