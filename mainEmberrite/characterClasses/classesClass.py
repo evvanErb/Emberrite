4e3914d8a7a1de07a7f15caa52333e8b61f8bcdd
evvanErb <evvan@protonmail.com>
2018-02-09 14:36:10 -0500

    minor health change

--- mainEmberrite/characterClasses/classesClass.py
+++ mainEmberrite/characterClasses/classesClass.py
@@ -11,11 +11,11 @@ class charClass():
 	#Determine health based on class
 	def healthReturn(self):
 		if (self.title == "rogue"):
-			health = 15
+			health = 20
 		elif (self.title == "hunter"):
 			health = 30
 		elif (self.title == "paladin"):
-			health = 45
+			health = 40
 		elif (self.title == "warrior"):
 			health = 35
 		return(health)
